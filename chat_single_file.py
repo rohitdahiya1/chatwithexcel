2e4f6d0984d7a002279711ecb9dc82a456687e0c
Rohit Dahiya <93477842+rohitdahiya1@users.noreply.github.com>
2025-06-04 18:04:36 +0530

    Create chat_single_file.py

---
+++ chat_single_file.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import pandasai as pai
+from pandasai_openai import AzureOpenAI
+import matplotlib.pyplot as plt
+
+AZURE_API_KEY       = "abf9b7951ac44ec187265d7054b8e804"
+AZURE_ENDPOINT      = "https://corp-openai-poc-tas-sm-01.openai.azure.com/"
+AZURE_DEPLOYMENT    = "gpt-4o"
+AZURE_API_VERSION   = "2024-08-01-preview"
+
+llm = AzureOpenAI(
+    api_token=AZURE_API_KEY,
+    azure_endpoint=AZURE_ENDPOINT,
+    deployment_name=AZURE_DEPLOYMENT,
+    api_version=AZURE_API_VERSION
+)
+pai.config.set({"llm": llm})
+
+df = pai.DataFrame({
+    "country": ["United States", "United Kingdom", "France", "Germany", "Italy", "Spain", "Canada", "Australia", "Japan", "China"],
+    "sales": [5000, 3200, 2900, 4100, 2300, 2100, 2500, 2600, 4500, 7000]
+    
+})
+
+
+
+response = pai.chat('Which are the top 5 countries by sales, respond in natural language in a sentence to a human',df)
+print("the type of response is" ,type(response))
+print("########################################")
+print("the generated code is", response.last_code_executed)
+print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")  # Convert response to dictionary for better readability
+print(response)
