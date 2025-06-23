b77aeab14253b7d9bf207bf7e1334b4a9a8384b2
Rohit Dahiya <93477842+rohitdahiya1@users.noreply.github.com>
2025-06-23 12:59:26 +0530

    Update chatwithdb.py

--- chatwithdb.py
+++ chatwithdb.py
@@ -25,10 +25,10 @@ class MyVanna(ChromaDB_VectorStore, OpenAI_Chat):
 vn = MyVanna(config={'model': 'gpt-4o'})
 
 
-SERVER = ''
-USERNAME = ''
-PASSWORD = ''
-DATABASE = ''
+SERVER = 'it-genaisupport.database.windows.net'
+USERNAME = 'it-genaisupport'
+PASSWORD = 'itgenai@123'
+DATABASE = 'it-genaisupport'
 
 
 def connect_to_mssql_with_pymssql():
@@ -62,6 +62,41 @@ if df_information_schema is not None:
     print(plan)
 else:
     print("Failed to retrieve schema.")
+
+vn.train(plan=plan)
+
+# The following are methods for adding training data. Make sure you modify the examples to match your database.
+# DDL statements are powerful because they specify table names, colume names, types, and potentially relationships
+vn.train(ddl="""
+    CREATE TABLE IF NOT EXISTS my-table (
+        id INT PRIMARY KEY,
+        name VARCHAR(100),
+        age INT
+    )
+""")
+
+# Sometimes you may want to add documentation about your business terminology or definitions.
+vn.train(documentation="Our business defines OTIF score as the percentage of orders that are delivered on time and in full")
+
+# You can also add SQL queries to your training data. This is useful if you have some queries already laying around. You can just copy and paste those from your editor to begin generating new SQL.
+vn.train(sql="SELECT * FROM my-table WHERE name = 'John Doe'")
+
+
+# df=vn.get_training_data() #to get all the training data
+
+## Asking the AI
+"Whenever you ask a new question, it will find the 10 most relevant pieces of training data and use it as part of the LLM prompt to generate the SQL."
+vn.ask(question= "",
+    print_results = True,
+    auto_train = True,
+    visualize= True,
+    allow_llm_to_see_data = False)
+
+# vn.generate_sql("What are the top 10 albums by sales?")
+# vn.remove_training_date(id="id") to remove particular training data
+# vn.get_similar_question_sql("Which artists have tracks in the 'Rock' genre?")
+# vn.run_sql("SELECT COUNT(*) AS TotalTables FROM sqlite_master WHERE type = 'table'")
+
 
 
 
